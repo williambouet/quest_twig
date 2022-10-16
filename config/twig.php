caed957517a9fd7f0aea798b929bc89311bf0a3a
WILLIAM BOUET <william.bouet.14@gmail.com>
2022-10-16 11:49:11 +0200

    added global

--- config/twig.php
+++ config/twig.php
@@ -5,3 +5,5 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
+$contactEmail = 'prenom@nom.com';
+$twig->addGlobal('contactEmail', $contactEmail);
